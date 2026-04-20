53a787e6db6f729140f7061a31c201ecb28e4a5d
ntan07 <nhattannn.3011@gmail.com>
2026-04-20 07:53:41 +0700

    Sao chep tep tin.

---
+++ CPP0801.cpp
@@ -0,0 +1,18 @@
+//
+// Created by ntan07 on 20/4/2026.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	freopen("PTIT.in", "r", stdin);
+	freopen("PTIT.out", "w", stdout);
+
+	string line;
+	while (getline(cin, line)) {
+		cout << line;
+		if (!cin.eof()) cout << '\n';
+	}
+
+	return 0;
+}
